a7f374000fbeec66d418c6a1db8886496ed56c3e
gopinathdhanakumar <gopimapala98@gmail.com>
2021-05-24 12:52:46 +0530

    master service

--- git/src/model/application.java
+++ git/src/model/application.java
@@ -3,6 +3,10 @@ package model;
 public class application {
 	public static void main(String[] args) {
 		System.out.println("hello hai");
+		System.out.println("master");
+		System.out.println("hello hai");
+		System.out.println("master");
+		
 	}
 
 }

f72e8de8bcd61e3cda8ae3ece2dd6be9c709ae5c
gopinathdhanakumar <gopimapala98@gmail.com>
2021-05-24 12:51:07 +0530

    branch service

--- git/src/model/application.java
+++ git/src/model/application.java
@@ -3,6 +3,11 @@ package model;
 public class application {
 	public static void main(String[] args) {
 		System.out.println("hello hai");
+		System.out.println("welcom to service");
+		System.out.println("welcom to service");
+		System.out.println("welcom to service");
+		System.out.println("welcom to service");
+		System.out.println("welcom to service");
 	}
 
 }
